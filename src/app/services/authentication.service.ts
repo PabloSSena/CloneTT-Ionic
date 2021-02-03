165d0bd196e9695548fb558bf36c04bb0ca0f9f6
PabloSSena <pablosilvasena@gmail.com>
2021-02-02 21:05:47 -0300

    Terminada tela de intro e começando a mexer na autenticaçao

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -1,9 +1,33 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import { Plugins } from '@capacitor/core';
+import { BehaviorSubject } from 'rxjs';
+const {Storage} = Plugins;
+const TOKENKEY ="my_token";
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
+  isAuthenticated: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(null);
+  token='';
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    this.loadToken();
+   }
+
+   async loadToken() {
+     const token = await Storage.get({key: TOKENKEY});
+     if(token && token.value) {
+       console.log('Set token: ', token.value);
+       this.token = token.value;
+       this.isAuthenticated.next(true);
+     }
+     else {
+       this.isAuthenticated.next(false);
+     }
+   }
+
+   login(credentials:{email,password}) {
+
+   }
 }
